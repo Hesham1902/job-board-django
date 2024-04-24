88ce49e97c3389ecad01e8c9086a10e89f8facfa
Hesham1902 <100668426+Hesham1902@users.noreply.github.com>
2024-04-24 11:56:22 +0200

    job models created

--- job/models.py
+++ job/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+JOB_TYPE = (
+('Full Time','Full Time'),
+    ('Part Time','Part Time')
+)
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    job_type = models.CharField(max_length=15,choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    Vacancy = models.IntegerField(default=1)
+    Salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    
+
+    def __str__(self) -> str:
+        return f'Need {self.Vacancy} {self.title} for {self.job_type} Job '
