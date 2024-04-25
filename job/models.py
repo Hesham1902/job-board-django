5fe59baf85fc0c385a34fe8284124bd051b27309
Hesham1902 <100668426+Hesham1902@users.noreply.github.com>
2024-04-25 10:33:28 +0200

    frontend integrated

--- job/models.py
+++ job/models.py
@@ -7,6 +7,11 @@ JOB_TYPE = (
     ('Part Time','Part Time')
 )
 
+def image_upload(instance, filename):
+    imagename, extension = filename.split(".")
+    return "jobs/%s.%s"%(instance.id, extension)
+
+
 class Job(models.Model):
     title = models.CharField(max_length=100)
     job_type = models.CharField(max_length=15,choices=JOB_TYPE)
@@ -15,7 +20,18 @@ class Job(models.Model):
     Vacancy = models.IntegerField(default=1)
     Salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
-    
+    categrory = models.ForeignKey('Category',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=image_upload)
+
 
     def __str__(self) -> str:
         return f'Need {self.Vacancy} {self.title} for {self.job_type} Job '
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    class Meta:
+        verbose_name_plural = "categories"
+    def __str__(self): 
+        return self.name
--- job/views.py
+++ job/views.py
@@ -1,3 +1,19 @@
+from django.http import Http404
 from django.shortcuts import render
+from job.models import Job
 
 # Create your views here.
+
+def job_list(request):
+    jobs = Job.objects.all()
+    for job in jobs:
+        print(job)
+    return render(request,'job/job_list.html',{"jobs": jobs})
+
+
+def job_detail(request, id):
+    try: 
+        job = Job.objects.get(pk=id)
+    except Job.DoesNotExist:
+        raise Http404('Job not found')
+    return render(request,'job/job_detail.html',{"job": job})
